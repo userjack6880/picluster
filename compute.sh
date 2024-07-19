67e57e0aafb904855ac96356b8c079faef96c2f0
userjack6880 <userjack6880@gmail.com>
2024-07-18 21:31:55 -0500

    compute and headnote scripts updated

--- compute.sh
+++ compute.sh
@@ -1,5 +1,5 @@
 # check for network first
-if ping -c 1 72.14.177.74 &> /dev/null
+if ping -c 1 5.161.201.170 &> /dev/null
 then
 
 	# update locales
@@ -7,24 +7,24 @@ then
 
 	# update repos
 
-	apt-get update
-	apt-get -y upgrade
-	apt-get -y install vim python3-distutils python3-dev
+	apt update
+	apt -y upgrade
+	apt -y install vim python3-distutils python3-dev
 
 	# create additional users
 	useradd -d /home/user -g users -M -p '$5$cOTJhkxlC4$kEFPIJaKPriv16lcwNBsS4dVMT1sC/a9vFPNlZDHug1' -s /bin/bash -u 1001 user
 
 	# copy /etc/hosts
-	cp /mnt/usb/configs/hosts /etc/hosts
+	cp -v /mnt/usb/configs/hosts /etc/hosts
 
 	# copy /etc/sudoers
-	cp /mnt/usb/configs/sudoers /etc/sudoers
+	cp -v /mnt/usb/configs/sudoers /etc/sudoers
 
 	# # copy dhcpcd.conf
-	cp /mnt/usb/configs/dhcpcd-compute.conf /etc/dhcpcd.conf
+	cp -v /mnt/usb/configs/dhcpcd-compute.conf /etc/dhcpcd.conf
 	sed -i "s/10.0.0.xx/10.0.0.$(($1+10))/g" /etc/dhcpcd.conf
-	chown root:netdev /etc/dhcpcd.conf
-	chmod 664 /etc/dhcpcd.conf
+	chown -v root:netdev /etc/dhcpcd.conf
+	chmod -v 664 /etc/dhcpcd.conf
 
 	# use nmcli to set static IP
 	# nmcli connection modify 'Wired connection 1' ipv4.address 10.0.0.$(($1+10))/24
--- headnode.sh
+++ headnode.sh
@@ -1,5 +1,5 @@
 # check for network first
-if ping -c 1 72.14.177.74 &> /dev/null
+if ping -c 1 5.161.201.170 &> /dev/null
 then
 
 	# update locales
@@ -7,9 +7,9 @@ then
 
 	# update repos
 
-	apt-get update
-	apt-get -y upgrade
-	apt-get -y install vim pdsh python3-distutils python3-dev
+	apt update
+	apt upgrade -y
+	apt -y install vim pdsh python3-distutils python3-dev
 
 	# configure pdsh
 	echo "ssh" > /etc/pdsh/rcmd_default
@@ -20,7 +20,7 @@ then
 	rm -r /home/admin
 
 	# create mount points
-	mkdir /mnt/apps
+	mkdir -v /mnt/apps
 
 	# if formatting disks, do it here
 	if [ "$1" == "format" ]; then
@@ -32,108 +32,109 @@ then
 	# add to fstab
 	blkid | grep /dev/sda1 | awk '{ print $5 "\t/home\text4\tdefaults\t0\t2" }' >> /etc/fstab
 	blkid | grep /dev/sda2 | awk '{ print $5 "\t/mnt/apps\text4\tdefaults\t0\t2" }' >> /etc/fstab
+  echo "fstab updated";
 
 	# mount all
+  echo "mounting partitions";
 	mount -a
 
 	# create symlinks
 
-	ln -s /mnt/apps /apps
+	ln -sv /mnt/apps /apps
 
 	# copy admin home back to the new /home
 
-	cp -r /var/tmp/admin /home
-	rm -r /var/tmp/admin
+	cp -vr /var/tmp/admin /home
+	rm -vr /var/tmp/admin
 
 	# fix permissions
 
-	chown -R admin:admin /home/admin
-	chown -R root:admin /mnt/apps
+	chown -vR admin:admin /home/admin
+	chown -vR root:admin /mnt/apps
 
-	chmod g+w /mnt/apps
+	chmod -v g+w /mnt/apps
 
 	# copy public/private keys
-	mkdir /home/admin/.ssh
-	cp /mnt/usb/configs/admin_privkey /home/admin/.ssh/id_rsa
-	cp /mnt/usb/configs/admin_pubkey /home/admin/.ssh/authorized_keys
-	chown -R admin:admin /home/admin/.ssh
-	chmod 600 /home/admin/.ssh/id_rsa
-	chmod 644 /home/admin/.ssh/authorized_keys
+	mkdir -v /home/admin/.ssh
+	cp -v /mnt/usb/configs/admin_privkey /home/admin/.ssh/id_rsa
+	cp -v /mnt/usb/configs/admin_pubkey /home/admin/.ssh/authorized_keys
+	chown -vR admin:admin /home/admin/.ssh
+	chmod -v 600 /home/admin/.ssh/id_rsa
+	chmod -v 644 /home/admin/.ssh/authorized_keys
 
 	# create additional users
 	useradd -d /home/user -g users -M -p '$5$cOTJhkxlC4$kEFPIJaKPriv16lcwNBsS4dVMT1sC/a9vFPNlZDHug1' -s /bin/bash -u 1001 user
-	mkdir /home/user
-	cp /home/admin/.bashrc /home/user/.bashrc
-	cp /home/admin/.profile /home/user/.profile
-	chown -R user:users /home/user
+	mkdir -v /home/user
+	cp -v /home/admin/.bashrc /home/user/.bashrc
+	cp -v /home/admin/.profile /home/user/.profile
+	chown -vR user:users /home/user
 
 	# download needed packages to /apps/pkgs
-	mkdir /mnt/apps/pkgs
-	rm -r /mnt/apps/pkgs/*.deb
-	rm /var/cache/apt/archives/*.deb
-	apt-get -y --download-only install nfs-kernel-server chrony
-	cp /var/cache/apt/archives/*.deb /mnt/apps/pkgs
+	mkdir -v /mnt/apps/pkgs
+	rm -v /var/cache/apt/archives/*.deb
+	apt -y --download-only install nfs-kernel-server chrony
+	cp -v /var/cache/apt/archives/*.deb /mnt/apps/pkgs
 
-	mkdir /mnt/apps/pkgs/mariadb-server
-	rm /var/cache/apt/archives/*.deb
-	apt-get -y --download-only install mariadb-server gawk
-	cp /var/cache/apt/archives/*.deb /mnt/apps/pkgs/mariadb-server
+	mkdir -v /mnt/apps/pkgs/mariadb-server
+	rm -v /var/cache/apt/archives/*.deb
+	apt -y --download-only install mariadb-server gawk
+	cp -v /var/cache/apt/archives/*.deb /mnt/apps/pkgs/mariadb-server
 
-	mkdir /mnt/apps/pkgs/slurm-head
-	rm /var/cache/apt/archives/*.deb
-	apt-get -y --download-only install slurm-wlm slurmdbd slurm-client 
-	cp /var/cache/apt/archives/*.deb /mnt/apps/pkgs/slurm-head
+	mkdir -v /mnt/apps/pkgs/slurm-head
+	rm -v /var/cache/apt/archives/*.deb
+	apt -y --download-only install slurm-wlm slurmdbd slurm-client 
+	cp -v /var/cache/apt/archives/*.deb /mnt/apps/pkgs/slurm-head
 
-	mkdir /mnt/apps/pkgs/slurm-compute
-	rm /var/cache/apt/archives/*.deb
-	apt-get -y --download-only install slurmd slurm-client
-	cp /var/cache/apt/archives/*.deb /mnt/apps/pkgs/slurm-compute
+	mkdir -v /mnt/apps/pkgs/slurm-compute
+	rm -v /var/cache/apt/archives/*.deb
+	apt -y --download-only install slurmd slurm-client
+	cp -v /var/cache/apt/archives/*.deb /mnt/apps/pkgs/slurm-compute
 
-	mkdir /mnt/apps/pkgs/openmpi
-	rm /var/cache/apt/archives/*.deb
-	apt-get -y --download-only install openmpi-bin openmpi-common libopenmpi-dev libopenmpi3 libltdl7
-	cp /var/cache/apt/archives/*.deb /mnt/apps/pkgs/openmpi
+	mkdir -v /mnt/apps/pkgs/openmpi
+	rm -v /var/cache/apt/archives/*.deb
+	apt -y --download-only install openmpi-bin openmpi-common libopenmpi-dev libopenmpi3 libltdl7
+	cp -v /var/cache/apt/archives/*.deb /mnt/apps/pkgs/openmpi
 
-	mkdir /mnt/apps/pkgs/glusterfs-server
-	rm /var/cache/apt/archives/*.deb
-	apt-get -y --download-only install glusterfs-server
-	cp /var/cache/apt/archives/*.deb /mnt/apps/pkgs/glusterfs-server
+	mkdir -v /mnt/apps/pkgs/glusterfs-server
+	rm -v /var/cache/apt/archives/*.deb
+	apt -y --download-only install glusterfs-server
+	cp -v /var/cache/apt/archives/*.deb /mnt/apps/pkgs/glusterfs-server
 
-	mkdir /mnt/apps/pkgs/glusterfs-client
-	rm /var/cache/apt/archives/*.deb
-	apt-get -y --download-only install glusterfs-client
-	cp /var/cache/apt/archives/*.deb /mnt/apps/pkgs/glusterfs-client
+	mkdir -v /mnt/apps/pkgs/glusterfs-client
+	rm -v /var/cache/apt/archives/*.deb
+	apt -y --download-only install glusterfs-client
+	cp -v /var/cache/apt/archives/*.deb /mnt/apps/pkgs/glusterfs-client
 
 	# mkdir /mnt/apps/pkgs/mpich
 	# rm /var/cache/apt/archives/*.deb
 	# apt-get -y --download-only install mpich
 	# cp /var/cache/apt/archives/*.deb /mnt/apps/pkgs/mpich
 
-	mkdir /mnt/apps/src
-	mkdir /mnt/apps/src/mpi4py
+	mkdir -v /mnt/apps/src
+	mkdir -v /mnt/apps/src/mpi4py
 	wget https://github.com/mpi4py/mpi4py/releases/download/3.1.5/mpi4py-3.1.5.tar.gz -P /apps/src/mpi4py
-	chown -R root:users /apps/src/mpi4py
-	chmod -R 770 /apps/src/mpi4py
+	chown -vR root:users /apps/src/mpi4py
+	chmod -vR 770 /apps/src/mpi4py
 
 	# make a copy of the files on this drive and save in apps/scripts and make it run only by root
-	cp -r /mnt/usb /mnt/apps/prep-scripts
-	chown -R root:root /mnt/apps/prep-scripts
-	chmod -R 700 /mnt/apps/prep-scripts
+	cp -vr /mnt/usb /mnt/apps/prep-scripts
+	chown -vR root:root /mnt/apps/prep-scripts
+	chmod -vR 700 /mnt/apps/prep-scripts
 
 	# copy /etc/hosts
-	cp /mnt/usb/configs/hosts /etc/hosts
+	cp -v /mnt/usb/configs/hosts /etc/hosts
 
 	# copy /etc/sudoers
-	cp /mnt/usb/configs/sudoers /etc/sudoers
+	cp -v /mnt/usb/configs/sudoers /etc/sudoers
 
 	# copy dhcpcd.conf
-	cp /mnt/usb/configs/dhcpcd-head.conf /etc/dhcpcd.conf
-	chown root:netdev /etc/dhcpcd.conf
-	chmod 664 /etc/dhcpcd.conf
+	cp -v /mnt/usb/configs/dhcpcd-head.conf /etc/dhcpcd.conf
+	chown -v root:netdev /etc/dhcpcd.conf
+	chmod -v 664 /etc/dhcpcd.conf
 
 	# copy chrony.conf to an accessible location
-	mkdir /mnt/apps/configs
-	cp /mnt/usb/configs/chrony-client.conf /mnt/apps/configs
+	mkdir -v /mnt/apps/configs
+	cp -v /mnt/usb/configs/chrony-client.conf /mnt/apps/configs
 
 	# wait
 	echo "done!"
