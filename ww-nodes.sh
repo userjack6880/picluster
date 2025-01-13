1b70218e492715469e2ecf3da33836e3b1deded7
Drew Helgerson <tah568@msstate.edu>
2025-01-13 13:41:41 -0600

    container should now build and import automagically

--- ww-nodes.sh
+++ ww-nodes.sh
@@ -1,22 +1,26 @@
-################################################################################
+###################################################################
 BASEDIR=$( dirname $0 )
-################################################################################
+###################################################################
 
-##################### container import ############################
-wwctl container import docker://ghcr.io/warewulf/warewulf-rockylinux:9 rockylinux-9-dracut
-wwctl container syncuser --write rockylinux-9-dracut
+### install docker(podman in a trenchcoat on rocky): ##############
+dnf install -y docker
 
-##################### container dracut setup ######################
-wwctl container exec rockylinux-9-dracut --bind /opt:/opt /bin/bash -c "\
-dnf update -y;
-dnf install -y /opt/warewulf/warewulf-dracut.rpm;
-dracut --force --no-hostonly --add wwinit --regenerate-all;
-exit
+### build container: ##############################################
+cd $BASEDIR/docker
+docker build . -t base-rocky9-dracut
+cd -
 
-################ profile setup (including dracut) #################
-wwctl profile set --container rockylinux-9-dracut default
+### import container to ww: #######################################
+# we'll store the intermediate file in a tmpfs
+mount -t tmpfs tmpfs /mnt 
+docker save base-rocky9-dracut /mnt/oci.tar
+wwctl container import /mnt/oci.tar base-rocky9-dracut
+umount /mnt
+
+### profile setup (including dracut) ##############################
+wwctl profile set --container base-rocky9-dracut default
 wwctl profile set --ipxe dracut default
 wwctl profile set --netdev eth0 default
 
-##################### add nodes ###################################
+### add nodes #####################################################
 wwctl node add pi-hpc-compute-[01-04] -I 10.0.0.11 --discoverable
